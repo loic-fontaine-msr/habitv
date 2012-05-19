5737610060c66c582bb21177b7d26ae5cec0dcdb
dabiboo <dabiboo@users.noreply.github.com>
2012-05-19 19:23:18 +0000

    Share project "RSSProvider" into "http://subversion.assembla.com/svn/habitv"

---
+++ RSSProvider/src/com/dabi/habitv/provider/rss/RSSConf.java
@@ -0,0 +1,13 @@
+package com.dabi.habitv.provider.rss;
+
+public final class RSSConf {
+	
+	private RSSConf(){
+		
+	}
+
+	public static final String NAME = "rss";
+	
+	public static final String DOWNLOADER = "aria2";
+
+}
