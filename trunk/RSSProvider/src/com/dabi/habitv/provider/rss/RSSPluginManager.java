deefd70ad1e54e0f8a5a0c5d6030a9463dee9f97
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2012-05-19 19:23:18 +0000

    Share project "RSSProvider" into "http://subversion.assembla.com/svn/habitv"

---
+++ trunk/RSSProvider/src/com/dabi/habitv/provider/rss/RSSPluginManager.java
@@ -0,0 +1,43 @@
+package com.dabi.habitv.provider.rss;
+
+import java.util.Set;
+
+import com.dabi.habitv.framework.plugin.api.ProviderPluginInterface;
+import com.dabi.habitv.framework.plugin.api.dto.CategoryDTO;
+import com.dabi.habitv.framework.plugin.api.dto.EpisodeDTO;
+
+public class RSSPluginManager implements ProviderPluginInterface {
+	
+	private ClassLoader classLoader;
+
+	@Override
+	public Set<EpisodeDTO> findEpisode(final CategoryDTO category) {
+		return RSSRetriever.findEpisodeByCategory(classLoader, category);
+	}
+
+	@Override
+	public Set<CategoryDTO> findCategory() {
+		return RSSCategoriesFinder.findCategory(classLoader); 
+	}
+
+	@Override
+	public String downloadCmd(final String url) {
+		return null;
+	}
+
+	@Override
+	public String getDownloader(final String url) {
+		return RSSConf.DOWNLOADER;
+	}
+
+	@Override
+	public String getName() {
+		return RSSConf.NAME;
+	}
+
+	@Override
+	public void setClassLoader(final ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
+}
