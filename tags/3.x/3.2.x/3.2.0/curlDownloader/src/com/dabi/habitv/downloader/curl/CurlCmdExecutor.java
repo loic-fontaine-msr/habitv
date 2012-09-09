2a9ca3f571417e283bc01360268d08e0d9b18b68
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2012-09-09 13:15:15 +0000

    trunk@205->3.2.0

---
+++ tags/3.x/3.2.x/3.2.0/curlDownloader/src/com/dabi/habitv/downloader/curl/CurlCmdExecutor.java
@@ -0,0 +1,36 @@
+package com.dabi.habitv.downloader.curl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.dabi.habitv.framework.plugin.utils.CmdExecutor;
+import com.dabi.habitv.framework.plugin.utils.CmdProgressionListener;
+
+public class CurlCmdExecutor extends CmdExecutor {
+
+	public CurlCmdExecutor(final String cmd, final CmdProgressionListener listener) {
+		super(cmd, listener);
+	}
+
+	@Override
+	protected String handleProgression(final String line) {
+		// % Total % Received % Xferd Average Speed Time Time Time Current
+		// 3 17.6M 3 561k 0 0 705k 0 0:00:25 --:--:-- 0:00:25 799k
+		final Pattern pattern = Pattern.compile("^\\s*(\\d+).*$");
+		final Matcher matcher = pattern.matcher(line);
+		// lancement de la recherche de toutes les occurrences
+		final boolean hasMatched = matcher.find();
+		// si recherche fructueuse
+		String ret = null;
+		if (hasMatched) {
+			ret = matcher.group(matcher.groupCount());
+		}
+		return ret;
+	}
+
+	@Override
+	protected boolean isSuccess(final String fullOutput) {
+		return getLastOutputLine().matches("^\\s*100\\s*.*$");
+	}
+
+}
