fec3fd5cd503b50add89ba6499c6a45a1577df43
dabiboo <dabiboo@users.noreply.github.com>
2012-09-27 05:46:57 +0000

    merge 3.2

--- m6w9/src/com/dabi/habitv/provider/m6w9/M6Conf.java
+++ m6w9/src/com/dabi/habitv/provider/m6w9/M6Conf.java
@@ -10,7 +10,6 @@ public final class M6Conf {
 	public static final String ENCRYPTION = "DES";
 	public static final String CATALOG_URL = "http://www.m6replay.fr/catalogue/catalogueWeb3.xml";
 	public static final String SECRET_KEY = "ElFsg.Ot";
-	public static final String DUMP_CMD = "-l 2 -n m6replayfs.fplive.net -a \"m6replaytoken/streaming?#TOKEN#\" -y \"#VIDEO_URL#?#TOKEN#\" --port 1935 --swfVfy \"http://l3.player.m6.fr/swf/ReplayPlayerV2Hds.swf\" --swfAge 0 -o  \"#FILE_DEST#\"";
-	public static final String TOKEN_URL = "http://www.m6replay.fr/tokenrtmp_2-8-10-2.php?cdn=lv3&videoUrl=";
+	public static final String DUMP_CMD = "-r \"rtmpe://groupemsix.fcod.llnwd.net/a2883/e1/#VIDEO_URL#?#TOKEN#\" -c 1935 -m 10 -o \"#FILE_DEST#\"";
 
 }
