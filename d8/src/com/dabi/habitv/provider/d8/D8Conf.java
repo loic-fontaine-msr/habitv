bb83af410941db2360d5d6f0015d394f0e2ba540
dabiboo <dabiboo@users.noreply.github.com>
2013-01-12 09:37:01 +0000

    refonte plugin D8

--- d8/src/com/dabi/habitv/provider/d8/D8Conf.java
+++ d8/src/com/dabi/habitv/provider/d8/D8Conf.java
@@ -6,9 +6,15 @@ interface D8Conf {
 
 	String EXTENSION = "mp4";
 
+	String RTMDUMP = "rtmpdump";
+
+	String RTMPDUMP_PREFIX = "rtmp:";
+
 	String CURL = "curl";
 
-	String CATALOG_URL = "http://www.d8.tv/rss/iphone/id/%s/type/program/";
-
 	int ROOT_CATEGORY_SIZE = 7;
+
+	String HOME_URL = "http://www.d8.tv";
+
+	String VIDEO_INFO_URL = "http://service.canal-plus.com/video/rest/getVideosLiees/d8/";
 }
--- d8/src/com/dabi/habitv/provider/d8/D8Retreiver.java
+++ d8/src/com/dabi/habitv/provider/d8/D8Retreiver.java
@@ -1,29 +1,35 @@
 package com.dabi.habitv.provider.d8;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.xml.bind.JAXBElement;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.dabi.habitv.framework.plugin.api.dto.CategoryDTO;
 import com.dabi.habitv.framework.plugin.api.dto.EpisodeDTO;
-import com.dabi.habitv.framework.plugin.exception.DownloadFailedException;
 import com.dabi.habitv.framework.plugin.exception.TechnicalException;
 import com.dabi.habitv.framework.plugin.utils.RetrieverUtils;
 
 class D8Retreiver {
-
-	private static final Pattern JS_URL_PATTERN = Pattern.compile("<script type=[\'|\"]text/javascript[\'|\"] src=[\'|\"]([^\'\"]*player-vod-ads.js.[^\'\"]*)[\'|\"]></script>");
-	private static final Pattern BASE_URL_PATTERN = Pattern.compile(".*baseUrl:\\s*\\\'(.*/)\\\',.*");
-	private static final Pattern FLV_URL_PATTERN = Pattern.compile(".*url : \\\'([^\\\']*)\\\'\\s*},");
 
 	private D8Retreiver() {
 
@@ -33,14 +39,14 @@ class D8Retreiver {
 		final Set<EpisodeDTO> episodes = new HashSet<>();
 
 		try {
-			final Connection con = Jsoup.connect(category.getId());
+			final Connection con = Jsoup.connect(D8Conf.HOME_URL+category.getId());
 
-			final Elements select = con.get().select(".home-replay-thumb");
-			for (final Element element : select) {
+			final Elements select = con.get().select(".list-programmes-emissions").get(0).children();
+			for (final Element liElement : select) {
 				try {
-					final Element aLink = element.child(2).child(0);
-					final String title = aLink.text();
-					final String url = aLink.attr("href");
+					final Element aLink = liElement.child(0);
+					final String title = aLink.child(1).child(0).text() + " - " + aLink.child(1).child(1).text();
+					final String url = aLink.attr("href").split("vid=")[1].split("&")[0];
 					episodes.add(new EpisodeDTO(category, title, url));
 				} catch (final IndexOutOfBoundsException e) {
 					throw new TechnicalException(e);
@@ -53,58 +59,79 @@ class D8Retreiver {
 	}
 
 	public static Set<CategoryDTO> findCategories(final ClassLoader classLoader) {
+		final Set<CategoryDTO> categories = new HashSet<>();
 
-		final Set<CategoryDTO> categoryDTOs = new HashSet<>();
-		for (int i = 0; i <= D8Conf.ROOT_CATEGORY_SIZE; i++) {
-			final Rubriques rubriques = (Rubriques) RetrieverUtils.unmarshalInputStream(
-					RetrieverUtils.getInputStreamFromUrl(String.format(D8Conf.CATALOG_URL, i)), Program.class.getPackage().getName(), classLoader);
-			final Rubrique rubrique = rubriques.getRubrique();
-			@SuppressWarnings("unchecked")
-			final CategoryDTO categoryDTO = new CategoryDTO(D8Conf.NAME, ((JAXBElement<String>) rubrique.getContent().get(1)).getValue(),
-					String.valueOf(rubrique.getId()), D8Conf.EXTENSION);
-			categoryDTOs.add(categoryDTO);
-			final Programs programs = (Programs) rubrique.getContent().get(3);
-			for (final Program program : programs.getProgram()) {
-				categoryDTO.addSubCategory(new CategoryDTO(D8Conf.NAME, program.getLabel(), program.getProgramUrlPartage(), D8Conf.EXTENSION));
+		try {
+			final Connection con = Jsoup.connect(D8Conf.HOME_URL);
+
+			final Elements select = con.get().select("#nav").get(0).child(0).children();
+			for (final Element liElement : select) {
+				final Element aElement = liElement.child(0);
+				final String url = aElement.attr("href");
+				final String name = aElement.text();
+				final CategoryDTO categoryDTO = new CategoryDTO(D8Conf.NAME, name, url, D8Conf.EXTENSION);
+				categoryDTO.addSubCategories(findSubCategories(url));
+				categories.add(categoryDTO);
+
 			}
+		} catch (final IOException e) {
+			throw new TechnicalException(e);
 		}
 
-		return categoryDTOs;
+		return categories;
 	}
 
-	public static String findEpisodeUrl(EpisodeDTO episode) throws DownloadFailedException {
-		String videoPage = RetrieverUtils.getUrlContent(episode.getUrl());
-		final Matcher matcher = JS_URL_PATTERN.matcher(videoPage);
-		final boolean hasMatched = matcher.find();
-		String jsUrl = null;
-		if (hasMatched) {
-			jsUrl = matcher.group(matcher.groupCount());
-			return findEpisodeUrlInJs(jsUrl, episode.getUrl());
-		} else {
-			throw new DownloadFailedException("can't find js url");
+	private static Collection<CategoryDTO> findSubCategories(final String catUrl) {
+		final Set<CategoryDTO> categories = new HashSet<>();
+
+		try {
+			final Connection con = Jsoup.connect(D8Conf.HOME_URL + catUrl);
+			final Elements select = con.get().select(".tp-grid").get(0).children();
+			for (final Element divElement : select) {
+				for (final Element subDivElement : divElement.children()) {
+					final Element aElement = subDivElement.child(0);
+					final String url = aElement.attr("href");
+					final String name = aElement.child(1).text();
+					final CategoryDTO categoryDTO = new CategoryDTO(D8Conf.NAME, name, url, D8Conf.EXTENSION);
+					categories.add(categoryDTO);
+				}
+
+			}
+		} catch (final IOException e) {
+			throw new TechnicalException(e);
 		}
 
+		return categories;
 	}
 
-	private static String findEpisodeUrlInJs(String jsUrl, String episodeUrl) throws DownloadFailedException {
-		String videoPage = RetrieverUtils.getUrlContentRef(jsUrl, episodeUrl);
-		Matcher matcher = BASE_URL_PATTERN.matcher(videoPage);
-		boolean hasMatched = matcher.find();
-		String baseUrl = null;
-		if (hasMatched) {
-			baseUrl = matcher.group(matcher.groupCount());
-		} else {
-			throw new DownloadFailedException("can't find baseUrl");
-		}
-		matcher = FLV_URL_PATTERN.matcher(videoPage);
-		hasMatched = matcher.find();
-		String flvUrl = null;
-		if (hasMatched) {
-			flvUrl = matcher.group(matcher.groupCount());
-		} else {
-			throw new DownloadFailedException("can't find flvUrl");
-		}
+	public static String findVideoUrl(final String id) {
+		try {
+			final DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+			domFactory.setNamespaceAware(true); // never forget this!
+			final DocumentBuilder builder = domFactory.newDocumentBuilder();
+			final Document doc = builder.parse(RetrieverUtils.getInputStreamFromUrl(D8Conf.VIDEO_INFO_URL + id));
 
-		return baseUrl + flvUrl;
+			final XPathFactory factory = XPathFactory.newInstance();
+			final XPath xpath = factory.newXPath();
+			final XPathExpression expr = xpath.compile("//VIDEO[ID='" + id + "']/MEDIA/VIDEOS");
+
+			final Object result = expr.evaluate(doc, XPathConstants.NODESET);
+			final NodeList nodes = ((NodeList) result).item(0).getChildNodes();
+			final Map<String, String> q2url = new HashMap<>();
+			for (int i = 0; i < nodes.getLength(); i++) {
+				q2url.put(nodes.item(i).getLocalName(), nodes.item(i).getTextContent());
+			}
+
+			String videoUrl = q2url.get("HD");
+			if (videoUrl == null) {
+				videoUrl = q2url.get("HAUT_DEBIT");
+			}
+			if (videoUrl == null) {
+				videoUrl = q2url.get("BAS_DEBIT");
+			}
+			return videoUrl;
+		} catch (IOException | XPathExpressionException | SAXException | ParserConfigurationException e) {
+			throw new TechnicalException(e);
+		}
 	}
 }
