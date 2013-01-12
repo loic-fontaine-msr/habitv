0257ba76155c790feddc6f10de22669c2cdd8278
dabiboo <dabiboo@users.noreply.github.com>
2013-01-12 09:36:06 +0000

    ajout timeout

--- lequipe/src/com/dabi/habitv/provider/lequipe/LEquipeRetreiver.java
+++ lequipe/src/com/dabi/habitv/provider/lequipe/LEquipeRetreiver.java
@@ -29,10 +29,9 @@ public class LEquipeRetreiver {
 
 	}
 
-	public static Set<EpisodeDTO> findEpisodeByCategory(
-			final CategoryDTO category) {
+	public static Set<EpisodeDTO> findEpisodeByCategory(final CategoryDTO category) {
 		final Set<EpisodeDTO> episodeList = new HashSet<>();
-		String baseUrl = LEquipeConf.VIDEO_HOME_URL + category.getId();
+		final String baseUrl = LEquipeConf.VIDEO_HOME_URL + category.getId();
 		findEpisodeByUrl(category, episodeList, baseUrl);
 		String pageUrl;
 		for (int i = 2; i <= LEquipeConf.MAX_PAGE; i++) {
@@ -43,22 +42,19 @@ public class LEquipeRetreiver {
 		return episodeList;
 	}
 
-	private static void findEpisodeByUrl(final CategoryDTO category,
-			final Set<EpisodeDTO> episodeList, String pageUrl) {
+	private static void findEpisodeByUrl(final CategoryDTO category, final Set<EpisodeDTO> episodeList, final String pageUrl) {
 		try {
-			final Connection con = Jsoup.connect(pageUrl);
+			final Connection con = Jsoup.connect(pageUrl).userAgent(LEquipeConf.USER_AGENT).timeout(15000);
 			Elements elementsByClass = con.get().getElementsByClass("content");
 			if (elementsByClass.isEmpty()) {
 				elementsByClass = con.get().getElementsByClass("results");
 				for (final Element aResult : elementsByClass.get(0).children()) {
 					final String hRef = aResult.attr("href");
-					final String name = aResult.getElementsByClass("title")
-							.text();
+					final String name = aResult.getElementsByClass("title").text();
 					episodeList.add(new EpisodeDTO(category, name, hRef));
 				}
 			} else {
-				final Elements divResults = elementsByClass.get(0).child(0)
-						.children();
+				final Elements divResults = elementsByClass.get(0).child(0).children();
 				for (final Element divResult : divResults) {
 					if (divResult.children().size() > 0) {
 						final Element aResult = divResult.child(0);
@@ -76,8 +72,7 @@ public class LEquipeRetreiver {
 	public static String findDownloadlink(final String url) {
 		final String originalUrl = LEquipeConf.VIDEO_HOME_URL + url;
 		final String htmlContent = RetrieverUtils.getUrlContent(originalUrl);
-		Pattern pattern = Pattern
-				.compile("<param name=\"flashVars\" value=\"language_code=fr&amp;playerKey=(.*)&amp[&amp]*;suffix=&amp;sig=(.*)\">");
+		Pattern pattern = Pattern.compile("<param name=\"flashVars\" value=\"language_code=fr&amp;playerKey=(.*)&amp[&amp]*;suffix=&amp;sig=(.*)\">");
 		Matcher matcher = pattern.matcher(htmlContent);
 		// lancement de la recherche de toutes les occurrences
 		boolean hasMatched = matcher.find();
@@ -88,8 +83,7 @@ public class LEquipeRetreiver {
 			playerKey = matcher.group(1);
 			sig = matcher.group(2);
 		} else {
-			pattern = Pattern
-					.compile("\\{\\\'sig\\\'\\:\\\'(.*)\\\',\\\'playerkey\\\':\\\'(.*)\\\',\\\'vformat\\\'\\:\\\'");
+			pattern = Pattern.compile("\\{\\\'sig\\\'\\:\\\'(.*)\\\',\\\'playerkey\\\':\\\'(.*)\\\',\\\'vformat\\\'\\:\\\'");
 			matcher = pattern.matcher(htmlContent);
 			// lancement de la recherche de toutes les occurrences
 			hasMatched = matcher.find();
@@ -102,40 +96,28 @@ public class LEquipeRetreiver {
 			}
 		}
 		try {
-			return findDownloadlinkBySigAndPlayerKey(sig, playerKey,
-					originalUrl);
+			return findDownloadlinkBySigAndPlayerKey(sig, playerKey, originalUrl);
 		} catch (final IOException e) {
 			throw new TechnicalException(e);
 		}
 	}
 
-	private static String findDownloadlinkBySigAndPlayerKey(final String sig,
-			final String playerKey, final String originalUrl)
-			throws IOException {
+	private static String findDownloadlinkBySigAndPlayerKey(final String sig, final String playerKey, final String originalUrl) throws IOException {
 		final String url = "http://api.kewego.com/config/getStreamInit/";
 		final URLConnection hc = (new URL(url)).openConnection();
-		hc.setRequestProperty("User-Agent",
-				"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1");
-		hc.setRequestProperty("X-KDORIGIN",
-				URLEncoder.encode(originalUrl, "UTF-8"));
-		String data = URLEncoder.encode("player_type", "UTF-8") + "="
-				+ URLEncoder.encode("kp", "UTF-8");
-		data += "&" + URLEncoder.encode("sig", "UTF-8") + "="
-				+ URLEncoder.encode(sig, "UTF-8");
-		data += "&" + URLEncoder.encode("playerKey", "UTF-8") + "="
-				+ URLEncoder.encode(playerKey, "UTF-8");
-		data += "&" + URLEncoder.encode("request_verbose", "UTF-8") + "="
-				+ URLEncoder.encode("false", "UTF-8");
-		data += "&" + URLEncoder.encode("language_code", "UTF-8") + "="
-				+ URLEncoder.encode("fr", "UTF-8");
+		hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1");
+		hc.setRequestProperty("X-KDORIGIN", URLEncoder.encode(originalUrl, "UTF-8"));
+		String data = URLEncoder.encode("player_type", "UTF-8") + "=" + URLEncoder.encode("kp", "UTF-8");
+		data += "&" + URLEncoder.encode("sig", "UTF-8") + "=" + URLEncoder.encode(sig, "UTF-8");
+		data += "&" + URLEncoder.encode("playerKey", "UTF-8") + "=" + URLEncoder.encode(playerKey, "UTF-8");
+		data += "&" + URLEncoder.encode("request_verbose", "UTF-8") + "=" + URLEncoder.encode("false", "UTF-8");
+		data += "&" + URLEncoder.encode("language_code", "UTF-8") + "=" + URLEncoder.encode("fr", "UTF-8");
 		hc.setDoOutput(true);
-		final OutputStreamWriter wr = new OutputStreamWriter(
-				hc.getOutputStream());
+		final OutputStreamWriter wr = new OutputStreamWriter(hc.getOutputStream());
 		wr.write(data);
 		wr.flush();
 		// Get the response
-		final BufferedReader rd = new BufferedReader(new InputStreamReader(
-				hc.getInputStream()));
+		final BufferedReader rd = new BufferedReader(new InputStreamReader(hc.getInputStream()));
 		String line;
 		final StringBuilder builder = new StringBuilder();
 		while ((line = rd.readLine()) != null) {
@@ -144,8 +126,7 @@ public class LEquipeRetreiver {
 		wr.close();
 		rd.close();
 
-		final Pattern pattern = Pattern
-				.compile("<playerAppToken>(.*)</playerAppToken>");
+		final Pattern pattern = Pattern.compile("<playerAppToken>(.*)</playerAppToken>");
 		final Matcher matcher = pattern.matcher(builder.toString());
 		// lancement de la recherche de toutes les occurrences
 		final boolean hasMatched = matcher.find();
@@ -156,24 +137,20 @@ public class LEquipeRetreiver {
 		} else {
 			throw new TechnicalException("can't find token");
 		}
-		return "\"http://api.kewego.com/video/getStream/?appToken=" + token
-				+ "&sig=" + sig + "&format=w640&v=2749\"";
+		return "\"http://api.kewego.com/video/getStream/?appToken=" + token + "&sig=" + sig + "&format=w640&v=2749\"";
 	}
 
 	public static Set<CategoryDTO> findCategory() {
 		try {
 			final Set<CategoryDTO> categoryDTOs = new HashSet<>();
-			final Connection con = Jsoup.connect(LEquipeConf.VIDEO_HOME_URL);
+			final Connection con = Jsoup.connect(LEquipeConf.VIDEO_HOME_URL).userAgent(LEquipeConf.USER_AGENT).timeout(15000);
 
-			final Elements aOngletsSport = con.get().select(".onglets_sports")
-					.get(0).children();
+			final Elements aOngletsSport = con.get().select(".onglets_sports").get(0).children();
 			for (final Element aHref : aOngletsSport) {
 				final String href = aHref.attr("href");
 				if (href.length() > 1) {
 					final String content = aHref.text();
-					final CategoryDTO categoryDTO = new CategoryDTO(
-							LEquipeConf.NAME, content, href,
-							LEquipeConf.EXTENSION);
+					final CategoryDTO categoryDTO = new CategoryDTO(LEquipeConf.NAME, content, href, LEquipeConf.EXTENSION);
 					categoryDTO.addSubCategories(findSubCategories(href));
 					categoryDTOs.add(categoryDTO);
 				}
@@ -185,22 +162,17 @@ public class LEquipeRetreiver {
 		}
 	}
 
-	private static Collection<CategoryDTO> findSubCategories(
-			final String categoryHref) {
+	private static Collection<CategoryDTO> findSubCategories(final String categoryHref) {
 		try {
 			final Set<CategoryDTO> categoryDTOs = new HashSet<>();
-			final Connection con = Jsoup.connect(LEquipeConf.VIDEO_HOME_URL
-					+ categoryHref);
+			final Connection con = Jsoup.connect(LEquipeConf.VIDEO_HOME_URL + categoryHref).userAgent(LEquipeConf.USER_AGENT).timeout(15000);
 
-			final Elements aOngletsSport = con.get().select("#k_sous").get(0)
-					.children();
+			final Elements aOngletsSport = con.get().select("#k_sous").get(0).children();
 			for (final Element aHref : aOngletsSport) {
 				final String href = aHref.attr("href");
 				if (href.length() > 1) {
 					final String content = aHref.text();
-					final CategoryDTO categoryDTO = new CategoryDTO(
-							LEquipeConf.NAME, content, href,
-							LEquipeConf.EXTENSION);
+					final CategoryDTO categoryDTO = new CategoryDTO(LEquipeConf.NAME, content, href, LEquipeConf.EXTENSION);
 					categoryDTOs.add(categoryDTO);
 				}
 
