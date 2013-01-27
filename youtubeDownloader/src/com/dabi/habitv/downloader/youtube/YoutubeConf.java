ff2a0fce8edb1a9da640ea8b2e9acb9a2a435a16
dabiboo <dabiboo@users.noreply.github.com>
2013-01-27 08:22:41 +0000

    intégration de youtube-dl

--- youtubeDownloader/src/com/dabi/habitv/downloader/youtube/YoutubeConf.java
+++ youtubeDownloader/src/com/dabi/habitv/downloader/youtube/YoutubeConf.java
@@ -8,6 +8,7 @@ public final class YoutubeConf {
 
 	public static final String NAME = "youtube";
 	public static final String ENCODING = "UTF-8";
-	public static final String USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13";
+	public static final String DUMP_CMD = " \"#VIDEO_URL#\" -o \"#FILE_DEST#\"";
+	public static final long MAX_HUNG_TIME = 300000L;
 
 }
