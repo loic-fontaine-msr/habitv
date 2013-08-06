ec6b96b50f0d17d54bb5bdfb1ab036f14da91a1d
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2013-08-06 22:27:07 +0000

    correction arte

--- trunk/arte/src/com/dabi/habitv/provider/arte/ArteConf.java
+++ trunk/arte/src/com/dabi/habitv/provider/arte/ArteConf.java
@@ -12,15 +12,14 @@ interface ArteConf {
 
 	String ID_EMISSION_TOKEN = "#ID_EMISSION#";
 
-	String RSS_CATEGORY_URL = "http://videos.arte.tv/fr/do_delegate/videos/programmes/" + ID_EMISSION_TOKEN + ",view,rss.xml";
+	String RSS_CATEGORY_URL = "http://videos.arte.tv/fr/do_delegate/videos/programmes/"
+			+ ID_EMISSION_TOKEN + ",view,rss.xml";
 
 	String ID_EPISODE_TOKEN = "#ID_EPISODE#";
 
-	String XML_INFO_URL = "http://videos.arte.tv/fr/do_delegate/videos/" + ID_EPISODE_TOKEN + ",view,asPlayerXml.xml";
-
-	String XML_INFO_URL2 = "http://videos.arte.tv/fr/do_delegate/videos/" + ID_EPISODE_TOKEN + ",view,strmVideoAsPlayerXml.xml";
-
 	String ENCODING = "UTF-8";
 
-	String RTMPDUMP_CMD = "-r \"#VIDEO_URL#\" -c 1935 -m 10 -w a8db5990d6ac5d4da6a23417206636053a8884a16db3b82e1ab3661a18d627f7 -x 966504 -o \"#FILE_DEST#\"";
+	String RTMPDUMP_CMD = "-r \"#VIDEO_URL#\" -c 1935 -m 10 -o \"#FILE_DEST#\"";
+
+	String CURL = "curl";
 }
