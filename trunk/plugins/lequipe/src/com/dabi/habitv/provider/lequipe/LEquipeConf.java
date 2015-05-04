7394fa46716de69c0fbf46de56c196aeb26c8148
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2015-05-04 20:19:58 +0000

    correction beinsport + lequipe

--- trunk/plugins/lequipe/src/com/dabi/habitv/provider/lequipe/LEquipeConf.java
+++ trunk/plugins/lequipe/src/com/dabi/habitv/provider/lequipe/LEquipeConf.java
@@ -8,6 +8,8 @@ public interface LEquipeConf {
 
 	String VIDEO_HOME_URL = "http://video.lequipe.fr";
 
+	String VIDEOS_URL = "http://video.lequipe.fr/morevideos";
+
 	String EXTENSION = FrameworkConf.MP4;
 
 	String USER_AGENT = "\"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)\"";
