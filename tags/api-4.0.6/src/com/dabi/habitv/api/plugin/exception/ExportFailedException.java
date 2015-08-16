941e73bf2d4800cec4640fad2d84cb8999d83fc6
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2015-08-16 16:06:25 +0000

    [maven-release-plugin]  copy for tag api-4.0.6

---
+++ tags/api-4.0.6/src/com/dabi/habitv/api/plugin/exception/ExportFailedException.java
@@ -0,0 +1,16 @@
+package com.dabi.habitv.api.plugin.exception;
+
+
+public class ExportFailedException extends ExecutorFailedException {
+
+	private static final long serialVersionUID = -4573862853212185451L;
+
+	public ExportFailedException(final ExecutorFailedException executorFailedException) {
+		super(executorFailedException.getCmd(), executorFailedException.getFullOuput(), executorFailedException.getLastLine(), executorFailedException);
+	}
+	
+	public ExportFailedException(final Exception exception) {
+		super("", exception.getMessage(), "", exception);
+	}
+
+}
