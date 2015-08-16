d6b6f0a8cd4e296748c95dd904e1e84523109cf2
dabiboo <dabiboo@users.noreply.github.com>
2015-08-16 16:49:00 +0000

    [maven-release-plugin]  copy for tag lequipe-4.0.8

--- fwk/framework/src/com/dabi/habitv/framework/plugin/utils/OSUtils.java
+++
@@ -1,9 +0,0 @@
-package com.dabi.habitv.framework.plugin.utils;
-
-public class OSUtils {
-	private static String OS = System.getProperty("os.name").toLowerCase();
-
-	public static boolean isWindows() {
-		return (OS.indexOf("win") >= 0);
-	}
-}
