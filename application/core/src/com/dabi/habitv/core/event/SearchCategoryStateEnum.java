ce1d3366d9d26968d8f5dd3d818e2b2e04c94243
dabiboo <dabiboo@users.noreply.github.com>
2015-08-16 16:44:05 +0000

    [maven-release-plugin]  copy for tag RSS-4.0.6

--- application/core/src/com/dabi/habitv/core/event/SearchCategoryStateEnum.java
+++
@@ -1,5 +0,0 @@
-package com.dabi.habitv.core.event;
-
-public enum SearchCategoryStateEnum {
-	IDLE, ERROR, BUILDING_CATEGORIES, DONE, CHANNEL_CATEGORIES_TO_BUILD, CATEGORIES_BUILT, STARTING, ALL_DONE;
-}
