939f5066147a65af9130bb88dc0c32e0965abad5
dabiboo <dabiboo@users.noreply.github.com>
2015-08-16 16:33:00 +0000

    [maven-release-plugin]  copy for tag adobeHDS-4.0.6

--- fwk/framework/src/com/dabi/habitv/framework/plugin/utils/SoccerUtils.java
+++
@@ -1,9 +0,0 @@
-package com.dabi.habitv.framework.plugin.utils;
-
-public class SoccerUtils {
-
-	public static String maskScore(String name) {
-		name = name.replaceAll("(\\d\\s*-\\s*\\d)", "").replaceAll("(\\d_*-_*\\d)", "");
-		return name;
-	}
-}
