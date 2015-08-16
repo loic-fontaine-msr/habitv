3de98b9253fcfa61a987ea0c36ffe39044a83e21
dabiboo <dabiboo@9257f6d0-930e-4d84-9059-13de012d0897>
2015-08-16 16:11:23 +0000

    [maven-release-plugin]  copy for tag framework-4.0.6

---
+++ tags/framework-4.0.6/src/com/dabi/habitv/framework/FrameworkConf.java
@@ -0,0 +1,47 @@
+package com.dabi.habitv.framework;
+
+public interface FrameworkConf {
+
+	String DOWNLOAD_INPUT = "#VIDEO_URL#";
+
+	String DOWNLOAD_DESTINATION = "#FILE_DEST#";
+
+	String EXTENSION = "#EXTENSION#";
+
+	String PARAMETER_ARGS = "ARGUMENTS";
+
+	String DOWNLOADER_PARAM = "downloader";
+
+	long TIME_BETWEEN_LOG = 2000L;
+
+	long HUNG_PROCESS_TIME = 180000L;
+
+	Integer TIME_OUT_MS = 30000;
+
+	String UPDATE_URL = "http://dabiboo.free.fr/repository";
+
+	String GROUP_ID = "com.dabi.habitv";
+
+	String VERSION = "version";
+
+	String RTMDUMP = "rtmpdump";
+
+	String CURL = "curl";
+
+	String MP4 = "mp4";
+
+	String FFMPEG = "ffmpeg";
+
+	String M3U8 = "m3u8";
+
+	String UTF8 = "UTF-8";
+
+	String DEFAULT_DOWNLOADER = CURL;
+
+	String ERROR_FILE = "dlError.index";
+
+	String USER_HOME = System.getProperty("user.home")
+			.replace("\\", "/");
+	
+	String ADOBEHDS = "adobeHDS";
+}
